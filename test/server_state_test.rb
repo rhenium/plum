561fd0ebb696deb0edcd07d0261cd078c4d14e77
Kazuki Yamaguchi <k@rhe.jp>
2015-07-24 12:05:08 +0900

    test: fix

--- test/server_state_test.rb
+++ test/server_state_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
+using Plum::BinaryString
+
 class ServerStateTest < Minitest::Test
   def test_server_must_repond_cprotocol_error_on_invalid_magic
     invalid_magic = "HELLO" * 10
     start_server do
       start_client do |sock|
         sock.write(invalid_magic)
-        ret = Plum::BinaryString.new(sock.readpartial(1024))
-        frame = Plum::Frame.parse!(ret)
+        frame =  nil
+        loop do
+          ret = sock.readpartial(1024)
+          frame = Plum::Frame.parse!(ret)
+          break if frame.type != :settings # server connection preface
+        end
         assert_equal(:goaway, frame.type) # connection error
         assert_equal(0x01, frame.payload.uint32(4)) # protocol error
       end
