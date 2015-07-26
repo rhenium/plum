bc518cfeab306faae046df9c2ca7a6e6aa1902f9
Kazuki Yamaguchi <k@rhe.jp>
2015-07-26 19:11:05 +0900

    test: hpack: huffman: test padding error

--- test/hpack_huffman_test.rb
+++ test/hpack_huffman_test.rb
@@ -12,4 +12,18 @@ class HPACKHuffmanTest < Minitest::Test
     expected = "\x10\x60\x2a\x1d\x94\x47\x82\x2c\x3d\x19\xbf\x8e\xd9\x24\xba\xe6\xb4\x1a\xe1\x5c\x39\x0f\x61\xf4\x3b\x08\x62\x54\x15\x0c"
     assert_equal(expected.force_encoding(Encoding::BINARY), Plum::HPACK::Huffman.decode(encoded).force_encoding(Encoding::BINARY))
   end
+
+  def test_too_long_padding
+    encoded = "\x1c\x64\xff\xff\xff"
+    assert_raises(Plum::HPACKError) {
+      Plum::HPACK::Huffman.decode(encoded)
+    }
+  end
+
+  def test_padding_not_filled
+    encoded = "\x1c\x71\xfe"
+    assert_raises(Plum::HPACKError) {
+      Plum::HPACK::Huffman.decode(encoded)
+    }
+  end
 end
