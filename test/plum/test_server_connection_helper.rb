47af906641a1764a2b75f9c1fc9919c8547cea14
Kazuki Yamaguchi <k@rhe.jp>
2015-08-06 11:18:41 +0900

    test: add test for ServerConnection#ping

--- test/plum/test_server_connection_helper.rb
+++ test/plum/test_server_connection_helper.rb
@@ -15,4 +15,14 @@ class ServerConnectionHelperTest < Minitest::Test
     }
   end
 
+  def test_server_ping
+    open_server_connection {|con|
+      con.ping("ABCABCAB")
+
+      last = sent_frames.last
+      assert_equal(:ping, last.type)
+      assert_equal([], last.flags)
+      assert_equal("ABCABCAB", last.payload)
+    }
+  end
 end
